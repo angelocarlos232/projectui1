402de8a2f829cf466474c2c4e9fcbbf6b81c80ae
juanpoks <141115652+juanpoks@users.noreply.github.com>
2024-01-30 04:06:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ function showOverlay2() {
 }
 
 function showOverlay3() {
-  document.getElementById('overlay2').classList.add('hidden');
-  document.getElementById('overlay4').classList.add('hidden');
-  document.getElementById('overlay3').classList.remove('hidden');
+  document.getElementById('overlay3').classList.add('hidden');
+  document.getElementById('overlay4').classList.remove('hidden');
   document.getElementById('main').classList.add('hidden');
 }
 
